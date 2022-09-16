edef2f26f0c1d1d4f2b9f812cfb31d7112c93579
Nicolas Estevez <esteveznicolas0@gmail.com>
2022-09-16 14:31:47 -0500

    Agrega modificaciones de estilos de interfaz de inicio de sesión

--- delivery_app/lib/src/login/login_page.dart
+++ delivery_app/lib/src/login/login_page.dart
@@ -15,49 +15,150 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Container(
         width: double.infinity,
-        child: Column(
+        child: Stack(
           children: [
-            Image.asset(
-                'assets/img/delivery.png',
-              width: 200,
-              height: 200,
+            Positioned(
+                top: -80,
+                left: -100,
+                child: _circleLoginSvg()
             ),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Correo Electronico'
-              ),
+            Positioned(
+                top: 60,
+                left: 25,
+                child: _loginText()
             ),
-            const TextField(
-              decoration: InputDecoration(
-                  hintText: 'Contraseña'
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () {},
-                child: Text('INGRESAR')
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
               children: [
-                Text(
-                    'No tienes Cuenta?',
-                    style: TextStyle(
-                      color: MyColors.primaryColor
-                    ),
-                ),
-                const SizedBox(width: 7,),
-                Text(
-                  'Registrate',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: MyColors.primaryColor
-                  )
-                )
-              ],
-            )
-          ]
+                _imageBanner(context),
+                _emailTextField(),
+                _passTextField(),
+                _loginButton(),
+                _rowElement()
+              ]
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+Widget _circleLoginSvg(){
+  return Container(
+    width: 240,
+    height: 230,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: MyColors.primaryColor
+    ),
+  );
+}
+
+Widget _imageBanner(BuildContext context) {
+
+  return Container(
+    margin: EdgeInsets.only(
+        top: 100,
+        bottom: MediaQuery.of(context).size.height * 0.22
+    ),
+    child: Image.asset(
+      'assets/img/delivery.png',
+      width: 200,
+      height: 200,
+    ),
+  );
+}
+
+Widget _emailTextField(){
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+    decoration: BoxDecoration(
+      color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: 'Correo Electronico',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(Icons.email, color: MyColors.primaryColor),
+          hintStyle: TextStyle(
+            color: MyColors.primaryColorDark
+          ),
+      ),
+    ),
+  );
+}
+
+Widget _passTextField(){
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+    decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(30)
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: 'Contraseña',
+        border: InputBorder.none,
+        contentPadding: EdgeInsets.all(15),
+        prefixIcon: Icon(Icons.lock, color: MyColors.primaryColor),
+        hintStyle: TextStyle(
+            color: MyColors.primaryColorDark
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _loginButton(){
+  return Container(
+    width: double.infinity,
+    margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+    child: ElevatedButton(
+        onPressed: () {},
+        child: Text('INGRESAR'),
+        style: ElevatedButton.styleFrom(
+          primary: MyColors.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30)
+          ),
+          padding: EdgeInsets.symmetric(vertical: 15)
+        ),
+    ),
+  );
+}
+
+Widget _loginText(){
+  return Text(
+      'LOGIN',
+    style: TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 22
+    ),
+  );
+}
+
+Widget _rowElement(){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        'No tienes Cuenta?',
+        style: TextStyle(
+            color: MyColors.primaryColor
+        ),
+      ),
+      const SizedBox(width: 7,),
+      Text(
+          'Registrate',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: MyColors.primaryColor
+          )
+      )
+    ],
+  );
+}
+
--- delivery_app/lib/src/utils/my_colors.dart
+++ delivery_app/lib/src/utils/my_colors.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 class MyColors{
   static Color primaryColor =  Color(0xFFE70D32);
+  static Color primaryColorDark =  Color(0xFFA7001C);
+  static Color primaryOpacityColor = Color.fromRGBO(231, 13, 50, 0.09);
 }
